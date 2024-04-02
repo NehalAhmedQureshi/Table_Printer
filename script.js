2f64992de1852b033233ef5cb4fc14ff63dce208
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-02 18:23:24 +0500

    error

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let form = document.querySelector("#tableForm");
+let form = document.querySelector("#inputForm");
 let input = document.querySelector("#tableInput");
-let print = document.querySelector("#printer");
+let table = document.querySelector("#tablePrinter");
 
 
 form.addEventListener("submit",(event ) => {
 
     event.preventDefault();
+
     let tableOf = input.value;   
-    print.innerHTML = "";
+    table.innerHTML = "";
     for (let i = 1; i <= 12; i++) {
 
         const message = `${tableOf} X ${i} = ${tableOf * i}`;
         table.innerHTML += `${message} <br>`;
         
 }
-}
-)
+})
